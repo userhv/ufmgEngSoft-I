c934fd9118ff1dc962fcc7ff360d51d70408d4b4
asafeclemente <asafecgm1@gmail.com>
2023-06-05 11:38:48 -0300

    Configuração votos nulos e brancos

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,16 @@ class Main {
         } else if(args[0].equals("PROD_2")){
             Produtos.PRODUTO_2 = true;
         } 
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("sem_nulo")) {
+                Produtos.NULO = false;
+            }
+            else if (args[i].equals("sem_branco")) {
+                Produtos.BRANCO = false;
+            }
+        }
+
 		View view = View.getInstance();
 		Controller controller = new Controller(view);		
         Controller.startSession();
--- src/Produtos.java
+++ src/Produtos.java
@@ -1,4 +1,7 @@
 public final class Produtos {
     public static boolean PRODUTO_1 = false;
     public static boolean PRODUTO_2 = false;
+
+    public static boolean NULO = true;
+    public static boolean BRANCO = true;
 }
